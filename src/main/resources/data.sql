3339d70b714cf2b82ea978c0aaec57315df40987
Josue Lobo <josuelobo2000@gmail.com>
2022-09-03 00:06:34 -0300

    Inicio de módulo 3

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '12345');
+
+INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('HTML', 'Front End');
+
